c5859fcf9db0efc43c6a41444b567cfa20afe3b5
adrian de seta <adriandeseta1985@gmail.com>
2025-07-28 15:14:01 -0300

    profile new ux

--- app/src/main/java/com/adriandeseta/cv/ui/adriandeseta/profile/ProfileScreen.kt
+++ app/src/main/java/com/adriandeseta/cv/ui/adriandeseta/profile/ProfileScreen.kt
@@ -2,13 +2,20 @@ package com.adriandeseta.cv.ui.adriandeseta.profile
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,12 +27,16 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.adriandeseta.cv.R
+import com.adriandeseta.cv.ui.main.resources.BoldText
+import com.adriandeseta.cv.ui.main.resources.CustomText
 
 @Composable
 fun ProfileScreen(modifier: Modifier, navController: NavController) {
@@ -33,38 +44,59 @@ fun ProfileScreen(modifier: Modifier, navController: NavController) {
         modifier = Modifier
             .fillMaxSize()
             .background(Color.White)
-            .padding(16.dp),
+            .padding(16.dp)
+            .verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
-        Text(
-            text = "Adrián De Seta - CV",
-            style = MaterialTheme.typography.headlineMedium,
-            modifier = Modifier
-                .padding(vertical = 32.dp)
-                .align(Alignment.CenterHorizontally)
+        BoldText(
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Start,
+            text = "Mi perfil",
+            fontSize = 70.sp
         )
 
-        Image(
-            painter = painterResource(R.drawable.cv_splash),
-            contentDescription = "Adrian De Seta CV",
-            modifier = Modifier
-                .size(150.dp)
-                .clip(CircleShape)
-                .background(MaterialTheme.colorScheme.secondary),
-            contentScale = ContentScale.FillBounds
+        HorizontalDivider(
+            color = Color.Black,
+            modifier = Modifier.padding(vertical = 16.dp)
         )
 
-        Spacer(Modifier.height(20.dp))
+        CustomText(
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Start,
+            text = "About",
+            fontSize = 12.sp,
+            fontWeight = FontWeight.Medium
+        )
 
-        Text(
+        Spacer(Modifier.height(10.dp))
+
+        CustomText(
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Start,
             text = stringResource(R.string.bio),
-            style = MaterialTheme.typography.bodyMedium.copy(
-                fontStyle = FontStyle.Italic
-            ),
-            textAlign = TextAlign.Justify,
-            modifier = Modifier.padding(16.dp)
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Medium
         )
+
+        Spacer(Modifier.height(10.dp))
+
+        Box(
+            modifier = Modifier.fillMaxWidth(),
+            contentAlignment = Alignment.TopStart
+        ) {
+            Image(
+                alignment = Alignment.CenterStart,
+                painter = painterResource(R.drawable.cv_home_pic),
+                contentDescription = "Adrian De Seta CV",
+                modifier = Modifier
+                    .width(198.dp)
+                    .height(247.dp)
+                    .clip(RoundedCornerShape(10.dp))
+                    .background(Color.White),
+                contentScale = ContentScale.FillBounds
+            )
+        }
     }
 }
 
